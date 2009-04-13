ba7afdefd6444dc269c1935b78caea4336af75ce
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-04-13 17:15:49 +0000

    Localisation updates for extension messages from translatewiki.net (2009-04-13 17:03 UTC)

--- ExtensionDistributor.i18n.php
+++ ExtensionDistributor.i18n.php
@@ -1255,6 +1255,7 @@ Wann De Frore övver dat Süßteem zom Zosätz erunger Lade haß, da jangk noh [
 $messages['lb'] = array(
 	'extensiondistributor' => 'MediaWiki Erweiderung eroflueden',
 	'extdist-desc' => "Erweiderung fir d'Verdeele vu Schnappschoss-Archive vun Erweiderungen",
+	'extdist-not-configured' => 'Konfiguréiert w.e.g. $wgExtDistTarDir an $wgExtDistWorkingCopy',
 	'extdist-no-such-extension' => 'Et gëtt keng Erweiderung "$1"',
 	'extdist-no-such-version' => 'D\'Erweiderung "$1" gëtt et net an der Versioun "$2".',
 	'extdist-choose-extension' => 'Wielt wat fir eng Erweiderung Dir wëllt eroflueden:',
