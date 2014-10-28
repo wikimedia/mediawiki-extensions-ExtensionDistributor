a052627fcb01f137a87546414d8cdd4ebf39376d
Brad Jorsch <bjorsch@wikimedia.org>
2014-10-28 16:04:33 -0400

    Add i18n for API module help
    MediaWiki core change I04b1a384 added support for i18n of API module
    help. This takes advantage of that while still maintaining backwards
    compatibility with earlier versions of MediaWiki.
    
    Once support for MediaWiki before 1.25 is dropped, the methods marked
    deprecated in this patch may be removed.
    
    Change-Id: I67395aff48185f3e09da31b51a08aa2541fe6a17

--- api/ApiListExtDistRepos.php
+++ api/ApiListExtDistRepos.php
@@ -28,13 +28,29 @@ class ApiListExtDistRepos extends ApiQueryGeneratorBase {
 		);
 	}
 
+	/**
+	 * @deprecated since MediaWiki core 1.25
+	 */
 	public function getDescription() {
 		return array(
 			'Returns the list of repositories supported by ExtensionDistributor'
 		);
 	}
 
+	/**
+	 * @deprecated since MediaWiki core 1.25
+	 */
 	public function getExamples() {
 		return 'api.php?action=query&list=extdistrepos';
+	}
+
+	/**
+	 * @see ApiBase::getExamplesMessages()
+	 */
+	protected function getExamplesMessages() {
+		return array(
+			'action=query&list=extdistrepos'
+				=> 'apihelp-query+extdistrepos-example-1',
+		);
 	}
 }
