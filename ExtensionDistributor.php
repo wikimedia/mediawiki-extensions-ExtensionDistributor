1083fb826996242f45eed558f87c69f05f5a7506
Kunal Mehta <legoktm@gmail.com>
2015-04-20 19:28:19 -0700

    Add extension.json, deprecate and empty PHP entry point
    Bug: T87913
    Change-Id: I7bc6a739f0b0367b89123d61e6b7dc600deb2fe8

--- ExtensionDistributor.php
+++ ExtensionDistributor.php
@@ -1,83 +1,15 @@
 <?php
 
-/**
- * This is an extension for distributing snapshot archives of extensions,
- * to be run on mediawiki.org
- */
-
-$wgExtensionCredits['specialpage'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'Extension Distributor',
-	'author'         => array( 'Tim Starling', 'Sam Reed', 'Chad Horohoe', 'Kunal Mehta' ),
-	'url'            => 'https://www.mediawiki.org/wiki/Extension:ExtensionDistributor',
-	'descriptionmsg' => 'extensiondistributor-desc',
-);
-
-/********************
- * Configuration
- */
-
-/**
- * Configuration for the API client to use
- *
- * Must have a 'class' key, can either be
- * "GithubExtDistProvider" or "GerritExtDistProvider"
- *
- * Common parameters:
- *  'apiUrl' - API url to use with $EXT and $REF variables
- *  'tarballUrl' - API url where tarballs are located
- *  'tarballName' - Expected filename of tarballs
- *  'proxy' - Proxy to use (optional)
- *
- * Github specific parameters:
- *  'token' - An OAuth token for authenticating requests
- *
- * Gerrit specific parameters:
- *  'repoListUrl' - API url to fetch a list of repositories
- *
- * @var array
- */
-$wgExtDistAPIConfig = false;
-
-/**
- * File to fetch list of extensions from, with one extension per line
- *
- * Example url: https://gerrit.wikimedia.org/mediawiki-extensions.txt
- */
-$wgExtDistListFile = false;
-
-/**
- * Supported branches/tags, master is the default (and shouldn't be removed)
- */
-$wgExtDistSnapshotRefs = array(
-	'master',
-);
-
-/**
- * Snapshot branch/tag to default to
- */
-$wgExtDistDefaultSnapshot = 'master';
-
-/********************
- * Registration
- */
-
-// Internationlization files
-$wgMessagesDirs['ExtensionDistributor'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['ExtensionDistributorAliases'] = __DIR__ . '/ExtensionDistributor.alias.php';
-
-// Special page classes
-$wgSpecialPages['ExtensionDistributor'] = 'SpecialExtensionDistributor';
-$wgSpecialPages['SkinDistributor'] = 'SpecialSkinDistributor';
-$wgSpecialPageGroups['ExtensionDistributor'] = 'developer';
-$wgSpecialPageGroups['SkinDistributor'] = 'developer';
-$wgAutoloadClasses['ExtensionDistributorHooks'] = __DIR__ .'/ExtensionDistributor.hooks.php';
-$wgAutoloadClasses['SpecialBaseDistributor'] = __DIR__ . '/specials/SpecialBaseDistributor.php';
-$wgAutoloadClasses['SpecialExtensionDistributor'] = __DIR__ . '/specials/SpecialExtensionDistributor.php';
-$wgAutoloadClasses['SpecialSkinDistributor'] = __DIR__ . '/specials/SpecialSkinDistributor.php';
-$wgAutoloadClasses['ExtDistProvider'] = __DIR__ . '/providers/ExtDistProvider.php';
-$wgAutoloadClasses['GerritExtDistProvider'] = __DIR__ . '/providers/GerritExtDistProvider.php';
-$wgAutoloadClasses['GithubExtDistProvider'] = __DIR__ . '/providers/GithubExtDistProvider.php';
-$wgAutoloadClasses['ApiListExtDistRepos'] = __DIR__ . '/api/ApiListExtDistRepos.php';
-$wgAPIListModules['extdistrepos'] = 'ApiListExtDistRepos';
-$wgHooks['APIQuerySiteInfoGeneralInfo'][] = 'ExtensionDistributorHooks::onAPIQuerySiteInfoGeneralInfo';
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'ExtensionDistributor' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['ExtensionDistributor'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['ExtensionDistributorAliases'] = __DIR__ . '/ExtensionDistributor.alias.php';
+	/* wfWarn(
+		'Deprecated PHP entry point used for ExtensionDistributor extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	); */
+	return;
+} else {
+	die( 'This version of the ExtensionDistributor extension requires MediaWiki 1.25+' );
+}
