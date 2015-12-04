f00d8701b645a72cf5709ee9c910b1f8ce6b6f81
Kunal Mehta <legoktm@member.fsf.org>
2015-12-03 20:28:11 -0800

    Get rid of ResourceLoaderRegisterModules hook
    The definition is fully static, so the hook is not required anymore.
    
    Change-Id: I202d9855ea482702339dd82fc2abe0661e549f26

--- ExtensionDistributor.hooks.php
+++ ExtensionDistributor.hooks.php
@@ -20,33 +20,4 @@ class ExtensionDistributorHooks {
 
 		return true;
 	}
-
-	/**
-	 * Hook to register our ResourceLoader module that depends upon configuration
-	 * @todo get rid of this hook
-	 * @param ResourceLoader $resourceLoader
-	 */
-	public static function onResourceLoaderRegisterModules( ResourceLoader &$resourceLoader ) {
-		$module = array(
-			'scripts' => 'ext.extensiondistributor.special.js',
-			'dependencies' => array(
-				'mediawiki.api',
-				'mediawiki.jqueryMsg',
-				'oojs-ui'
-			),
-			'messages' => array(
-				'extdist-choose-version-extensions',
-				'extdist-choose-version-skins',
-				'extdist-submit-version',
-				'extdist-no-versions-extensions',
-				'extdist-no-versions-skins',
-				'extdist-branch-alpha',
-				'extdist-branch-stable',
-				'extdist-branch-candidate',
-			),
-			'localBasePath' => __DIR__ . '/resources',
-			'remoteExtPath' => 'ExtensionDistributor/resources',
-		);
-		$resourceLoader->register( 'ext.extensiondistributor.special', $module );
-	}
 }
